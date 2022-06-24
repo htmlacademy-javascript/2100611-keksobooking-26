d1c7060672eb6c5adb40311aebab568d1b019483
Konstantin <106699099+Konstantintolkachev@users.noreply.github.com>
2022-06-25 01:19:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
-function randomInteger (min, max){
+function randomInteger (min, max) {
   let rand = min - 0.5 + Math.random() * (max - min + 1);
-  return Math.ruond (rand);
+  return Math.round (rand);
+}
+alert(randomInteger(1, 3));
+function getRandomArbitrary (min, max, maxDigits = 0) {
+  if (min > max || min < 0 || max <= 0) {
+    throw new Error('Задан неверный диапазон! Укажите другие числа.');
   }
-  alert ( randomInteger(1, 3));
-  //https://learn.javascript.ru/task/random-int-min-max
-
-  function getRandomArbitrary(min, max, maxDigits = 0) {
-    if (min > max || min < 0 || max <= 0) {
-      return ('Задан неверный диапазон! Укажите другие числа.');
-    }
-
-    const digitsDegree = 10 * maxDigits;
-    return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
-  }
-  getRandomArbitrary(1, 2, 6);
+  let digitsDegree = 10 * maxDigits;
+  return ~~((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+}
+getRandomArbitrary(1, 2, 6);
